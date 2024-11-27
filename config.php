dbcc3a171217e05ee5577b48cf0bf715ca8912f0
Paul Waring <paul@xk7.net>
2024-11-27 13:32:37 +0000

    Move config to separate file

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(E_ALL);
+
+set_error_handler(
+    function ($severity, $message, $file, $line) {
+        throw new \ErrorException($message, $severity, $severity, $file, $line);
+    }
+);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dsn = 'sqlite:' . __DIR__ . '/htdocs/books.db';
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+];
+$db = new PDO($dsn, null, null, $options);
--- import.php
+++ import.php
@@ -4,21 +4,7 @@ declare(strict_types=1);
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-error_reporting(E_ALL);
-
-set_error_handler(
-    function ($severity, $message, $file, $line) {
-        throw new \ErrorException($message, $severity, $severity, $file, $line);
-    }
-);
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$dsn = 'sqlite:' . __DIR__ . '/htdocs/books.db';
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-];
-$db = new PDO($dsn, null, null, $options);
+require_once __DIR__ . '/config.php';
 
 $schema = file_get_contents(__DIR__ . '/books.sql');
 $db->exec($schema);
